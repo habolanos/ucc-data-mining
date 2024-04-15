76bb00b285876a80c783a613f7dcac9a7442326b
JohanC29 <jc.muelasvernaza@gmail.com>
2024-04-14 20:25:01 -0500

    The workshop is held in data mining class with pandas and numpy

--- students/jcmv-2k24s1/python/20240405-dmEjercicioPanda.py
+++ students/jcmv-2k24s1/python/20240405-dmEjercicioPanda.py
@@ -8,7 +8,7 @@ absolutepath = os.path.abspath(__file__)
 fileDirectory = os.path.dirname(absolutepath)
 
 # Se obtiene la ruta del archivo a leer
-pahtFile = fileDirectory +'/dataInput/09-clase-20240312/06.Lobbyist_Data_-_Historical_-_Lobbyist_Registry_-_2010_20240312.csv'
+pahtFile = fileDirectory +'/../dataInput/09-clase-20240312/06.Lobbyist_Data_-_Historical_-_Lobbyist_Registry_-_2010_20240312.csv'
 
 # Carga de datos
 data = pd.read_csv(pahtFile)
