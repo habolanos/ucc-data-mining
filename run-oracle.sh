4eea64d044c6debc8dd67a4e4bc2c8c511e312fc
Harold Adrian <adrian@e11evenn.co>
2024-10-07 19:55:52 -0500

    Ejercicion Pentaho

--- run-oracle.sh
+++ run-oracle.sh
@@ -1,1 +1,1 @@
-docker run --name oracle--dm -p 1521:1521 -e APP_USER=oracle -e APP_USER_PASSWORD=oracle -e ORACLE_DATABASE=ORCL -e ORACLE_PASSWORD=oracle -d gvenzl/oracle-free
+docker run --name datamining -p 1521:1521 -e APP_USER=datamining -e APP_USER_PASSWORD=oracle -e ORACLE_DATABASE=ORCL -e ORACLE_PASSWORD=oracle -d gvenzl/oracle-free
